0e87aa0e95d3b0e2bf1e1e1a6549dcc3e3b17135
whits15 <80317938+whits15@users.noreply.github.com>
2022-04-18 22:10:35 -0400

    Add files via upload
    SEO has been done on all static pages
    
    shopping cart is now working relatively well
    
    img folder has been cleaned up

--- shopping-cart.js
+++ shopping-cart.js
@@ -1,16 +1,20 @@
 class cartItem{
-    constructor(price, name, img){
+    constructor(id, price, name, img){
+        this.id = id
         this.name = name;
         this.price = price;
         this.img = img;
         this.qty = 1;
     }
-    add_one(){
-        this.qty++;
-    }
+}
+if (localStorage) {
+    var items = new Map();
+    var cart_items = new Map();
+}
+else{
+    console.log("local storage error");
 }
 
-var items = new Map();
 var total = 0.00;
 var row_total = 1;
 var products_in_cart = 0;
@@ -19,104 +23,133 @@ var products_in_cart = 0;
 use push to add items
 remake shoping cart
 change the params then build the object in function
+add to cart function works
+make cart does not
 */
-
-function newAddCart(item){
-    var cart = document.getElementById("cart_body");
-    item = new cartItem(10.99,"test","img/women/pr2.jpg")
-    total+=item.price;
-    var content =` 
-    <tr>
-        <td class="shoping__cart__item">
-            <img src="${item.img}" alt="product image">
-            <h5>${item.name}</h5>
-        </td>
-        <td class="shoping__cart__price">
-            $${item.price}
-        </td>
-        <td class="shoping__cart__quantity">
-            <div class="quantity">
-                <div class="pro-qty">
-                    <input type="number" value="1" min="1">
-                </div>
-            </div>
-        </td>
-        <td class="shoping__cart__total">
-        $${total}
-        </td>
-        <td class="shoping__cart__item__close">
-            <span class="icon_close" onclick="removeItem(${row_total},${item.price})"></span>
-        </td>
-    </tr>`
-    items.set(item.name,content);
-    var row = document.createElement('tr');
-    row.innerHTML = items.get(item.name);
-    cart.append(row);
+function addToCart(id,price,name,img){
+    console.log("added!");
+    if (localStorage.getItem("items") == '' || localStorage.getItem("items")==null) {
+        localStorage.setItem("items",'[]');
+    }
+    var storage = JSON.parse(localStorage.getItem("items"));
+    var prod = new cartItem(id,price,name,img);
+    storage.push(prod);
+    localStorage.setItem("items",JSON.stringify(storage));
     products_in_cart++;
-    document.getElementById("products-in-cart").innerHTML = products_in_cart;
-    document.getElementById("cart-subtotal").innerHTML ="$"+ (Math.round(total * 100) / 100).toFixed(2);
-    document.getElementById("cart-total").innerHTML = "$"+(Math.round(total * 1.13 * 100) / 100).toFixed(2);
-    console.log("adding item" + item.name);
-}
-
-
-function updateCart(item){
-    var cart = document.getElementById("cart_body");
-        item = new cartItem(10.99,"test","img/cart/cart3.jpg")
-        total+=item.price;
-        var content =` 
-        <tr>
-        <td class="shoping__cart__item">
-            <img src="${item.img}" alt="product image">
-            <h5>${item.name}</h5>
-        </td>
-        <td class="shoping__cart__price">
-            $${item.price}
-        </td>
-        <td class="shoping__cart__quantity">
-            <div class="quantity">
-                <div class="pro-qty">
-                    <input type="number" value="1">
+    
+    var content =` 
+        <tr id="${prod.name}">
+            <td class="shoping__cart__item">
+                <img src="${prod.img}" alt="product image">
+                <h5>${prod.name}</h5>
+            </td>
+            <td class="shoping__cart__price">
+                $${prod.price}
+            </td>
+            <td class="shoping__cart__quantity">
+                <div class="quantity">
+                    <div class="pro-qty">
+                        <input type="number" value="1" min="1">
+                    </div>
                 </div>
-            </div>
-        </td>
-        <td class="shoping__cart__total">
-        $${total}
-    </td>
-    <td class="shoping__cart__item__close">
-        <span class="icon_close" onclick="rItem(${item.name})"></span>
-    </td>
-</tr>`
-        row_total++;
+            </td>
+            <td class="shoping__cart__total">
+            $${prod.price}
+            </td>
+            <td class="shoping__cart__item__close">
+                <span class="icon_close" onclick="removeItem('${prod.id}')"></span>
+            </td>
+        </tr>`
+        var page = document.getElementById("cart_body");
+        total+=prod.price;
         var row = document.createElement('tr');
         row.innerHTML = content;
-        cart.append(row);
+        page.append(row);
+        products_in_cart++;
+        document.getElementById("products-in-cart").innerHTML = products_in_cart;
         document.getElementById("cart-subtotal").innerHTML ="$"+ (Math.round(total * 100) / 100).toFixed(2);
         document.getElementById("cart-total").innerHTML = "$"+(Math.round(total * 1.13 * 100) / 100).toFixed(2);
-        document.getElementById("total-from-cart").innerHTML = "$"+ (Math.round(total * 100) / 100).toFixed(2);
-        console.log("adding item");
-        console.log(total);
+        console.log("adding item" + prod.name);
 }
 
-function rItem(name) {
-    items.delete(name);
-    for (let i = 1; i < row_total; i++) {
-        document.getElementsByTagName("tr")[i].remove();
+function makeCart() {
+    document.getElementsByClassName("cart_body").innerHTML = "";
+    console.log("running!");
+    var page = document.getElementById("cart_body");
+    temp = localStorage.getItem("items");
+    var cart = JSON.parse(temp);
+    console.log(cart);
+    for (let i = 0; i < cart.length; i++) {
+        const product = cart[i];
+        var content =` 
+        <tr id="${product.name}">
+            <td class="shoping__cart__item">
+                <img src="${product.img}" alt="product image">
+                <h5>${product.name}</h5>
+            </td>
+            <td class="shoping__cart__price">
+                $${product.price}
+            </td>
+            <td class="shoping__cart__quantity">
+                <div class="quantity">
+                    <div class="pro-qty">
+                        <input type="number" value="1" min="1">
+                    </div>
+                </div>
+            </td>
+            <td class="shoping__cart__total">
+            $${product.price}
+            </td>
+            <td class="shoping__cart__item__close">
+                <span class="icon_close" onclick="removeItem('${product.id}')"></span>
+            </td>
+        </tr>`
+        total+=product.price;
+        var row = document.createElement('tr');
+        row.innerHTML = content;
+        page.append(row);
+        products_in_cart++;
+        document.getElementById("products-in-cart").innerHTML = products_in_cart;
+        document.getElementById("cart-subtotal").innerHTML ="$"+ (Math.round(total * 100) / 100).toFixed(2);
+        document.getElementById("cart-total").innerHTML = "$"+(Math.round(total * 1.13 * 100) / 100).toFixed(2);
+        console.log("adding item" + product.name);
+    
     }
-    row_total =1;
-    total = 0;
-    var products_in_cart = 0;
-    items.forEach(element => {
-        updateCart(element);
-    });
 }
+function removeItem(id){
+    console.log("working");
+    
+    var storage = JSON.parse(localStorage.getItem("items"));
 
-function removeItem(num,price){
-    document.getElementsByTagName("tr")[num].remove();
-    total -= price;
+    total = 0;
+    var new_storage = new Array();
+    console.log("the id is " + id);
+    for (let i = 0; i < storage.length; i++) {
+        var element = storage[i];
+        console.log(element);
+        if (element.id != id) {
+            new_storage.push(element);
+        }
+        else{
+            total-=element.price;
+        }
+    }
+    console.log(new_storage);  
+    localStorage.clear();
+    localStorage.setItem("items",JSON.stringify(new_storage));
+        document.getElementById("cart_body").innerHTML = "";
+    products_in_cart = 0;
+    for (let i = 0; i < new_storage.length; i++) {
+        const element = new_storage[i];
+        total+=element.price;
+        products_in_cart++;
+        addToCart(element.id,element.price,element.name,element.img);
+    }
+    
     products_in_cart--;
     document.getElementById("products-in-cart").innerHTML = products_in_cart;
     document.getElementById("cart-subtotal").innerHTML ="$"+ (Math.round(total * 100) / 100).toFixed(2);
     document.getElementById("cart-total").innerHTML = "$"+(Math.round(total * 1.13 * 100) / 100).toFixed(2);
     console.log(total);
+    
 }
